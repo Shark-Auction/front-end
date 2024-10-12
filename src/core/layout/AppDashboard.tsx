33cb9caa7fa63e5cc8c93606645b1075bebefe70
namhuu6969 <namquangbinh6969@gmail.com>
2024-10-12 09:51:49 +0700

    re css for admin dashboard

--- src/core/layout/AppDashboard.tsx
+++ src/core/layout/AppDashboard.tsx
@@ -1,10 +1,23 @@
 import type { MenuProps } from "antd";
-import { Avatar, Dropdown, Layout, Menu, theme } from "antd";
+import { Dropdown, Layout, Menu, theme } from "antd";
 import React, { useState } from "react";
-import { AiFillProduct } from "react-icons/ai";
-import { MdCategory, MdDashboard } from "react-icons/md";
+import { AiFillPieChart, AiFillProduct } from "react-icons/ai";
+import {
+  MdBrandingWatermark,
+  MdCategory,
+  MdGroups2,
+  MdManageAccounts,
+  MdNoAccounts,
+  MdOutlineAccountCircle,
+} from "react-icons/md";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 
+import { BiSolidDiscount } from "react-icons/bi";
+import { FaBlogger } from "react-icons/fa";
+import { FaUserPen } from "react-icons/fa6";
+import { IoLogOutSharp } from "react-icons/io5";
+import { PiShoppingCartFill } from "react-icons/pi";
+import { RiAccountCircleFill, RiAuctionFill } from "react-icons/ri";
 import { TfiStatsUp } from "react-icons/tfi";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/slice/userSlice";
@@ -30,32 +43,53 @@ function getItem(
 
 const items: MenuItem[] = [
   {
-    key: '',
-    label: 'Dashboard',
-    icon: <MdDashboard />,
-    children: [
-      getItem("Statistics", "statistic", <TfiStatsUp />),
-    ],
+    key: "",
+    label: "Dashboard",
+    icon: <AiFillPieChart size={20} />,
+    children: [getItem("Statistics", "statistic", <TfiStatsUp size={20} />)],
   },
   {
-    key: 'menu',
-    label: 'Account Management',
-    icon: <MdCategory />,
+    key: "menu",
+    label: "Account Management",
+    icon: <RiAccountCircleFill size={20} />,
     children: [
-      getItem("All User", "account-management", <MdCategory />),
-      getItem("Staff", "staff-management", <MdCategory />),
-      getItem("Manager", "manager-management", <MdCategory />),
-
+      getItem("All User", "account-management", <MdGroups2 size={20} />),
+      getItem("Staff", "staff-management", <FaUserPen size={20} />),
+      getItem("Manager", "manager-management", <MdManageAccounts size={20} />),
     ],
   },
-  getItem("Category Management", "category-management", <MdCategory />),
-  getItem("Auction Management", "auction-management", <AiFillProduct />),
-  getItem("Brand Management", "brand-management", <AiFillProduct />),
-  getItem("Origin Management", "origin-management", <AiFillProduct />),
-  getItem("Product Management", "product-management", <AiFillProduct />),
-  getItem("Blog Management", "blog-management", <AiFillProduct />),
-  getItem("Violate Management", "violate-management", <AiFillProduct />),
-  getItem("voucher Management", "voucher-management", <AiFillProduct />),
+  getItem(
+    "Category Management",
+    "category-management",
+    <MdCategory size={20} />
+  ),
+  getItem(
+    "Auction Management",
+    "auction-management",
+    <RiAuctionFill size={20} />
+  ),
+  getItem("Brand Management", "brand-management", <AiFillProduct size={20} />),
+  getItem(
+    "Origin Management",
+    "origin-management",
+    <MdBrandingWatermark size={20} />
+  ),
+  getItem(
+    "Product Management",
+    "product-management",
+    <PiShoppingCartFill size={20} />
+  ),
+  getItem("Blog Management", "blog-management", <FaBlogger size={20} />),
+  getItem(
+    "Violate Management",
+    "violate-management",
+    <MdNoAccounts size={20} />
+  ),
+  getItem(
+    "voucher Management",
+    "voucher-management",
+    <BiSolidDiscount size={20} />
+  ),
 ];
 
 const AppDashboard: React.FC = () => {
@@ -72,12 +106,29 @@ const AppDashboard: React.FC = () => {
   };
   const itemsUser: MenuProps["items"] = [
     {
-      key: "1",
-      label: <Link to={"/u/profile"}>Thông tin</Link>,
+      key: "infor",
+      label: (
+        <div className="flex flex-col gap-1">
+          <p className="text-lg font-bold">
+            {userLogin && userLogin["fullName"]}
+          </p>
+          <p className="font-normal text-gray-500 text-base">
+            Role:{" "}
+            <span className="text-blue-600">
+              {userLogin && userLogin["roleName"]}
+            </span>
+          </p>
+        </div>
+      ),
     },
     {
-      key: "2",
-      label: <div onClick={handleLogin}>Đăng xuất</div>,
+      key: "Logout",
+      label: (
+        <div className="text-red-500 text-base" onClick={handleLogin}>
+          Logout
+        </div>
+      ),
+      icon: <IoLogOutSharp className="text-red-500" size={20} />,
     },
   ];
   return (
@@ -86,6 +137,7 @@ const AppDashboard: React.FC = () => {
         collapsible
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
+        width={270}
       >
         <div className="demo-logo-vertical" />
         <Menu
@@ -93,6 +145,7 @@ const AppDashboard: React.FC = () => {
           defaultSelectedKeys={["1"]}
           mode="inline"
           items={items}
+          className="!text-base"
         />
       </Sider>
       <Layout>
@@ -104,17 +157,10 @@ const AppDashboard: React.FC = () => {
                 placement="bottomLeft"
                 arrow
                 trigger={["click"]}
+                className="cursor-pointer hover:opacity-70 duration-300"
+                overlayStyle={{ width: 200 }}
               >
-                <div className="flex items-center gap-2 border border-gray-400 py-1 px-5 rounded-lg">
-                  <Avatar size={"large"} />
-                  <div className="flex flex-col">
-                    <p className="text-sm font-bold">
-                      <span>{userLogin["fullName"]}</span>
-                      <br />
-                      <span className="font-normal text-gray-500">{userLogin["roleName"]}</span>
-                    </p>
-                  </div>
-                </div>
+                <MdOutlineAccountCircle size={40} />
               </Dropdown>
             )}
           </div>
