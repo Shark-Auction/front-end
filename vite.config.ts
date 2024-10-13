1272894ecf433ee2c7b635664a7fe0a872f082d1
namhuu6969 <namquangbinh6969@gmail.com>
2024-10-13 19:16:02 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-// Sử dụng __dirname nếu gặp lỗi với import.meta.url
-const root = path.resolve(__dirname, 'src');
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': root,
-      '@components': path.resolve(root, 'components'),
-      '@axios': path.resolve(root, 'config/axios'),
-      '@layout': path.resolve(root, 'core/layout'),
-      '@store': path.resolve(root, 'core/store'),
-      '@hooks': path.resolve(root, 'hooks'),
-      '@model': path.resolve(root, 'model'),
-      '@pages': path.resolve(root, 'pages'),
-      '@service': path.resolve(root, 'service'),
-      '@utils': path.resolve(root, 'utils'),
-    },
-  },
   server: {
     host: true,
   },
@@ -29,4 +11,4 @@ export default defineConfig({
     assetsDir: 'assets',
     emptyOutDir: true,
   },
-});
+})
